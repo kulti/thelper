889452fdb07723f84c4471ea5fe4735fd55b508c
Aleksey Bakin <alexey.bakin@gmail.com>
2021-01-08 11:40:17 +0300

    fix: filter sub-test functions from thelper checks

---
+++ pkg/analyzer/testdata/src/t/s.go
@@ -0,0 +1,14 @@
+package t
+
+import "testing"
+
+func BenchmarkSubtestWithAnonymous(b *testing.B) {
+	b.Run("sub", func(b *testing.B) {})
+}
+
+func BenchmarkSubtest(b *testing.B) {
+	b.Run("sub", check)
+}
+
+func check(b *testing.B) {
+}
