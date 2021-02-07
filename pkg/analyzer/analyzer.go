3ab400d39bf21ce71da6a61b8754338648f3a8b2
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-02-07 03:09:21 +0100

    review

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -26,7 +26,7 @@ Available checks
 ` + checkTName + `  - check *testing.T param has t name
 
 Also available similar checks for benchmark and TB helpers: ` +
-		checkBBegin + `, ` + checkBFirst + `, ` + checkBName +
+		checkBBegin + `, ` + checkBFirst + `, ` + checkBName + `,` +
 		checkTBBegin + `, ` + checkTBFirst + `, ` + checkTBName + `
 
 `
