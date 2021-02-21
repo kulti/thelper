1905be40b8575871202797a5a6e49dd6398b057b
Aleksey Bakin <alexey.bakin@gmail.com>
2021-02-21 18:27:02 +0300

    feat: calc call count to separate subtest from helper

--- pkg/analyzer/testdata/src/t/s.go
+++ pkg/analyzer/testdata/src/t/s.go
@@ -7,8 +7,28 @@ func TestSubtestWithAnonymous(t *testing.T) {
 }
 
 func TestSubtest(t *testing.T) {
+	h := subhelper{}
 	t.Run("sub", check)
+	t.Run("sub sel", h.check)
+}
+
+func TestSubtestWithHelperAsRun(t *testing.T) {
+	h := subhelper{}
+	t.Run("sub with helper", anotherCheck)
+	t.Run("sub sel", h.anotherCheck)
 }
 
 func check(t *testing.T) {
+	anotherCheck(t)
 }
+
+func anotherCheck(t *testing.T) { // want "test helper function should start from t.Helper()"
+}
+
+type subhelper struct{}
+
+func (sh subhelper) check(t *testing.T) {
+	sh.anotherCheck(t)
+}
+
+func (sh subhelper) anotherCheck(t *testing.T) {} // want "test helper function should start from t.Helper()"
